3701b83c77b983cd26e295bb3df838e80a75185b
Stephen Ribbon <stribbon@icloud.com>
2015-10-02 19:59:27 -0700

    fixed confirm control

--- src/client/controllers/ConfirmCtrl.js
+++ src/client/controllers/ConfirmCtrl.js
@@ -1,13 +1,13 @@
-// module.exports = ['$http', '$scope', '$state', 'User', 'Cart', '$cookieStore', '$cookies', function($http, $scope, $state, User, Cart, $cookieStore, $cookies) {
-//   // GET DEMO USER
-//   $scope.user = User.getUser();
-//   // GET ITEMS IN CART
-//   // $scope.cart = Cart.getItems();
-//   $scope.cart = Cart.getMyCart();
-//   // TOTAL ITEMS IN CART
-//   $scope.Badge = Cart.getTotalItemsInCart();
-//   // TOTAL PRICE OF CART
-//   $scope.Total = Cart.addTotalPrice($scope.cart);
-//   // REMOVE ITEM FROM CART
+module.exports = ['$http', '$scope', '$state', 'User', 'Cart', '$cookieStore', '$cookies', function($http, $scope, $state, User, Cart, $cookieStore, $cookies) {
+  // GET DEMO USER
+  $scope.user = User.getUser();
+  // GET ITEMS IN CART
+  // $scope.cart = Cart.getItems();
+  $scope.cart = Cart.getMyCart();
+  // TOTAL ITEMS IN CART
+  $scope.Badge = Cart.getTotalItemsInCart();
+  // TOTAL PRICE OF CART
+  $scope.Total = Cart.addTotalPrice($scope.cart);
+  // REMOVE ITEM FROM CART
   
-// }];
+}];
