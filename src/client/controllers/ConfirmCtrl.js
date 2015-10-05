7e7b88323351105542fd8e38a60a8ed9d55f1a3c
Stephen Ribbon <stribbon@icloud.com>
2015-10-05 10:14:11 -0700

    fixed modal columns

--- src/client/controllers/ConfirmCtrl.js
+++ src/client/controllers/ConfirmCtrl.js
@@ -6,6 +6,21 @@ module.exports = ['$http', '$scope', '$state', 'User', 'Cart', 'Order', '$cookie
   $scope.Total = Cart.addTotalPrice();
   $scope.order = $cookieStore.get('orderInfo');
   $scope.ETA = $cookieStore.get('ETA');
+  // var eta = Number(ETA.charAt(0));
+  // setInterval(function() {
+  //   if(eta == 1){
+  //     eta = 'Your wine should arrive at any moment!';
+  //     $scope.ETA = eta;
+  //   }
+  //   else if (eta > 0){
+  //     eta -= 1;
+  //     $scope.ETA = eta;
+  //   } else {
+  //     eta = 'Your wine should arrive any moment!';
+  //     $scope.ETA = eta;
+  //   }
+  //     console.log(eta);
+  // }, 1000);
   // $http.get('/api/orders/' + orderId ).then(res => {
   //   $scope.order = res.data;
   // });
