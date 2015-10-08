7fb8bd21042e0a8adadaa33d2c1f2b79069a9f43
Stephen Ribbon <stribbon@icloud.com>
2015-10-08 10:56:30 -0700

    confirm page added blue and reorganizing

--- src/client/controllers/ConfirmCtrl.js
+++ src/client/controllers/ConfirmCtrl.js
@@ -1,28 +1,9 @@
 module.exports = ['$http', '$scope', '$state', 'User', 'Cart', 'Order', '$cookieStore', '$cookies', '$stateParams', function($http, $scope, $state, User, Cart, Order, $cookieStore, $cookies, $stateParams) {
-  // GET DEMO USER
+  // GET DATA
   $scope.user = User.getUser();
-
   $scope.cart = Cart.getMyCart();
   $scope.Total = Cart.addTotalPrice();
   $scope.order = $cookieStore.get('orderInfo');
   $scope.ETA = $cookieStore.get('ETA');
-  // var eta = Number(ETA.charAt(0));
-  // setInterval(function() {
-  //   if(eta == 1){
-  //     eta = 'Your wine should arrive at any moment!';
-  //     $scope.ETA = eta;
-  //   }
-  //   else if (eta > 0){
-  //     eta -= 1;
-  //     $scope.ETA = eta;
-  //   } else {
-  //     eta = 'Your wine should arrive any moment!';
-  //     $scope.ETA = eta;
-  //   }
-  //     console.log(eta);
-  // }, 1000);
-  // $http.get('/api/orders/' + orderId ).then(res => {
-  //   $scope.order = res.data;
-  // });
-  
+
 }];
